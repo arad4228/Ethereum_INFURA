9a9359ec26224bb2b9935fd0f6608d0de7df745c
arad4228 <arad4228@gmail.com>
2024-01-17 13:05:27 +0900

    [KyoungJae]Fix allocate API-Key logic(Thread to Process)

--- replica_eth.py
+++ replica_eth.py
@@ -16,9 +16,7 @@ base_Output_PATH = "./output/"
 MAX_Chunk_Number = 362
 MAX_Quiry = 100000
 MAX_Thread_Quiry = 20000
-used_urls = set()
-# INFURA_URL_Limit_List = {key: 0 for key in INFURA_URL_List}
-# Second_INFURA_URL_Limit_List = {key: 0 for key in Second_INFURA_URL_List}
+MAX_Process_Quiry = 80000
 
 def is_eoa(w3, address, shared_URL_Limit_List, lock):
     try:
@@ -37,50 +35,42 @@ def is_eoa(w3, address, shared_URL_Limit_List, lock):
 def find_available_url(shared_URL_Limit_List, used_urls, lock):
     with lock:
         for url in INFURA_URL_List:
-            if used_urls[url] == 0 and MAX_Quiry - shared_URL_Limit_List[url] >= MAX_Thread_Quiry and shared_URL_Limit_List[url] < MAX_Quiry:
+            if used_urls[url] == 0 and MAX_Quiry - shared_URL_Limit_List[url] >= MAX_Process_Quiry:
                 used_urls[url] = 1
                 return url
 
         raise Exception("All API Keys expired or reached limit")
 
 
-def work_thread(ith, chunk_df, return_df, shared_URL_Limit_List, used_urls, lock):
-    url_INFURA = find_available_url(shared_URL_Limit_List, used_urls, lock)
+def work_thread(ith, chunk_df, return_df, url_INFURA, lock):
     w3 = Web3(Web3.HTTPProvider(base_URL + url_INFURA))
     print(f"{threading.current_thread().name} Start")
-    print(f"Using API-Key:{url_INFURA}")
 
     chunk_df = chunk_df.copy()
     chunk_df['from_address'] = chunk_df['from_address'].apply(w3.to_checksum_address)
     chunk_df['to_address'] = chunk_df['to_address'].apply(w3.to_checksum_address)
 
-    while True:
-        try:
-            eoa_df = chunk_df[
-                (chunk_df['from_address'].apply(lambda x: is_eoa(w3, x, shared_URL_Limit_List, lock))) & 
-                (chunk_df['to_address'].apply(lambda x: is_eoa(w3, x, shared_URL_Limit_List, lock)))
-            ]
-            with lock:
-                used_urls[url_INFURA] = 0
-            print(f"{threading.current_thread().name} Done")
-            return_df.insert(ith, eoa_df)
-            break
-        except Exception as e:
-            if "All API Keys" in str(e):
-                raise Exception("All API Keys expired or reached limit")
-            else:
-                with lock:
-                    url_INFURA = find_available_url(shared_URL_Limit_List, used_urls, lock)
-                print(f'Change the API Key to {url_INFURA}')
-                w3 = Web3(Web3.HTTPProvider(base_URL + url_INFURA))
+    try:
+        eoa_df = chunk_df[
+            (chunk_df['from_address'].apply(lambda x: is_eoa(w3, x, shared_URL_Limit_List, lock))) & 
+            (chunk_df['to_address'].apply(lambda x: is_eoa(w3, x, shared_URL_Limit_List, lock)))
+        ]
+        with lock:
+            used_urls[url_INFURA] = 0
+        print(f"{threading.current_thread().name} Done")
+        return_df.insert(ith, eoa_df)
+
+    except Exception as e:
+        raise Exception(e)
 
 
 def refine_INFURA(nProcess, file_Name, shared_URL_Limit_List, used_urls, lock):
-    print(f"Process{nProcess} start reading {file_Name}")
     file_Path = base_Input_PATH + file_Name
     chunk_df = pd.read_csv(file_Path)
+    url_INFURA = find_available_url(shared_URL_Limit_List, used_urls, lock)
+    print(f"Process{nProcess} start reading {file_Name}")
+    print(f"Process{nProcess} Using API-Key:{url_INFURA}")
 
-    #chunk_df = pd.read_csv("C:\\Users\\PET\\Desktop\\논문작성관련\\self-trade\\230914\\result_1.csv")
     # Drop rows with NaN or empty values in 'from_address' or 'to_address'
     chunk_df = chunk_df.dropna(subset=['from_address', 'to_address'])
     thread_List = []
@@ -91,12 +81,7 @@ def refine_INFURA(nProcess, file_Name, shared_URL_Limit_List, used_urls, lock):
     
     for nThread in range(4):
         end = start + addition_Count
-        with lock:
-            all_limits_exceeded = all(shared_URL_Limit_List[url] >= MAX_Quiry for url in INFURA_URL_List)
-        if all_limits_exceeded:
-            raise Exception("All API Keys expired")
-
-        thread = Thread(target=work_thread, args=(nThread, chunk_df.loc[start:end], output_df_list, shared_URL_Limit_List, used_urls, lock))
+        thread = Thread(target=work_thread, args=(nThread, chunk_df.loc[start:end], output_df_list, url_INFURA, lock))
         thread_List.append(thread)
         start += addition_Count
         thread.start()
@@ -121,10 +106,12 @@ if __name__ == "__main__":
         shared_URL_Limit_List = manager.dict({key: 0 for key in INFURA_URL_List})
         used_urls = manager.dict({key: 0 for key in INFURA_URL_List})
         lock = manager.Lock()
-        for i in range(5, MAX_Chunk_Number, 2):
+
+        for i in range(5, MAX_Chunk_Number, len(INFURA_URL_List)):
                 process_List = []
                 try:
-                    for nProcess in range(2):
+                    # Infura URL 갯수 만큼 코어를 늘려 할당한다.
+                    for nProcess in range(len(INFURA_URL_List)):
                         file_name = f"chunk_{i + nProcess}.csv"
                         process = Process(target=refine_INFURA, args=(nProcess + 1, file_name, shared_URL_Limit_List, used_urls,lock))
                         process_List.append(process)
